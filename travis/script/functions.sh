52c3bd8138e1cd55b6a9255530720f27427ba060
Myron Marston <myron.marston@gmail.com>
2014-01-24 19:36:31 -0800

    Don't run cukes on projects that lack them.
    rspec-support doesn't have any.

--- travis/script/functions.sh
+++ travis/script/functions.sh
@@ -60,24 +60,26 @@ function run_specs_and_record_done {
 }
 
 function run_cukes {
-  # force jRuby to use client mode JVM or a compilation mode thats as close as possible,
-  # idea taken from https://github.com/jruby/jruby/wiki/Improving-startup-time
-  #
-  # Note that we delay setting this until we run the cukes because we've seen
-  # spec failures in our spec suite due to problems with this mode.
-  export JAVA_OPTS='-client -XX:+TieredCompilation -XX:TieredStopAtLevel=1'
+  if [ -d features ]; then
+    # force jRuby to use client mode JVM or a compilation mode thats as close as possible,
+    # idea taken from https://github.com/jruby/jruby/wiki/Improving-startup-time
+    #
+    # Note that we delay setting this until we run the cukes because we've seen
+    # spec failures in our spec suite due to problems with this mode.
+    export JAVA_OPTS='-client -XX:+TieredCompilation -XX:TieredStopAtLevel=1'
 
-  if is_mri_192; then
-    # For some reason we get SystemStackError on 1.9.2 when using
-    # the bin/cucumber approach below. That approach is faster
-    # (as it avoids the bundler tax), so we use it on rubies where we can.
-    bundle exec cucumber --strict
-  else
-    # Prepare RUBYOPT for scenarios that are shelling out to ruby,
-    # and PATH for those that are using `rspec` or `rake`.
-    RUBYOPT="-I${PWD}/../bundle -rbundler/setup" \
-       PATH="${PWD}/bin:$PATH" \
-       bin/cucumber --strict
+    if is_mri_192; then
+      # For some reason we get SystemStackError on 1.9.2 when using
+      # the bin/cucumber approach below. That approach is faster
+      # (as it avoids the bundler tax), so we use it on rubies where we can.
+      bundle exec cucumber --strict
+    else
+      # Prepare RUBYOPT for scenarios that are shelling out to ruby,
+      # and PATH for those that are using `rspec` or `rake`.
+      RUBYOPT="-I${PWD}/../bundle -rbundler/setup" \
+         PATH="${PWD}/bin:$PATH" \
+         bin/cucumber --strict
+    fi
   fi
 }
 
